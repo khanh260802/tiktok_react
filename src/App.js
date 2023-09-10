3f0e0f5d7dbb44a4603b051ce545b6a49c1832e5
Yeany1 <dinhbaoyeny2002@gmail.com>
2023-09-10 16:05:08 +0700

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    
+    const cars = ['Lamboghini', 'Honda', 'Mazda']; 
+    const [options, setOptions] = useState([]); 
+    function handlerCheckbox(id) { 
+        if(options.find((car)=> car === cars[id]))
+            setOptions(options.filter((car, index) => index !== id))
+        else 
+            setOptions([...options, cars[id]]); 
+    }
+    console.log(options)
+    return (
+        <div className="App">
+            {cars.map((car, index) => 
+                <div key = {index}>
+                    <label htmlFor={car}> {car} </label>
+                    <input
+                        id={index} 
+                        type="checkbox"
+                        onChange={() => handlerCheckbox(index)}
+                    />
+                </div>
+            )}
+            
+        </div>
+    );      
 }
 
 export default App;
