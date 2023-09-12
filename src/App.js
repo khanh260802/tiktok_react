6116fd43caa48ab82813fe5f8eb071b615b25008
khanh260802 <casiofx580@gmail.com>
2023-09-12 15:17:14 +0700

    very simple totolist react

--- src/App.js
+++ src/App.js
@@ -1,48 +1,36 @@
 import "./App.css";
 import { useState } from "react";
 function App() {
-    const cars = [
-        {
-            name: "Lamboghini",
-            id: 1,
-        },
-        {
-            name: "Honda",
-            id: 2,
-        },
-        {
-            name: "Mazda",
-            id: 3,
-        },
-    ];
-    const [options, setOptions] = useState([]);
-    var handlerSubmid = () => {
-        console.log(options);
-    };
 
-    var handlerCheckox = (id) => {
-        setOptions((prev) => {
-            if (!prev.includes(id)) 
-                return [...prev, id];
-            else 
-                return prev.filter((itemID) => itemID !== id);
-        });
-    };
+    console.log(localStorage); 
+    const [job, setJob] = useState(''); 
+    const [jobs, setJobs] = useState(() => { 
+        const localStorageJobs = JSON.parse(localStorage.getItem('todoList')); 
+        return localStorageJobs ?? []; 
+    }); 
+
+    const handlerAddJob = () => { 
+        setJobs((prev) => {
+            const newJobs = [...prev, job]; 
+            localStorage.setItem('todoList', JSON.stringify(newJobs)); 
+            return newJobs; 
+        }); 
+        setJob(''); 
+    }
 
     return (
         <div className="App">
-            {cars.map((car) => (
-                <div key={car.id}>
-                    <label htmlFor={car.id}> {car.name} </label>
-                    <input
-                        id={car.id}
-                        type="checkBox"
-                        onChange={() => handlerCheckox(car.id)}
-                        checked = {options.includes(car.id)}
-                    />
-                </div>
-            ))}
-            <button onClick={handlerSubmid}> Submit </button>
+            <input
+                value={job}
+                onChange={(e)=>setJob(e.target.value)}
+            /> 
+            
+            <button
+                onClick={handlerAddJob}
+            > ADD </button>
+            <ul>
+                {jobs.map((item, index) => <li key={index}> {item} </li>)}
+            </ul>
         </div>
     );
 }
